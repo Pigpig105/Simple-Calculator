789d5039bde19a45248db0df65d5d746b96a5ad7
Pigpig105 <81789882+Pigpig105@users.noreply.github.com>
2022-06-11 21:47:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,51 +11,13 @@ document.getElementById("additionButton").onclick = function(){
     document.getElementById("additionLabel").innerHTML = sum;
 }
 
-let subtraction1;
-let subtraction2;
-let difference;
+let rounding;
+let exponent;
+let square;
+let division;
+let seven;
 
-document.getElementById("subtractionButton").onclick = function(){
-    subtraction1 = document.getElementById("subtraction1").value;
-    subtraction2 = document.getElementById("subtraction2").value;
-    subtraction1 = Number(subtraction1);
-    subtraction2 = Number(subtraction2);
-    difference = subtraction1 - subtraction2;
-    document.getElementById("subtractionLabel").innerHTML = difference;
-}
-
-let multiplication1;
-let multiplication2;
-let product;
-
-document.getElementById("multiplicationButton").onclick = function(){
-    multiplication1 = document.getElementById("multiplication1").value;
-    multiplication2 = document.getElementById("multiplication2").value;
-    multiplication1 = Number(multiplication1);
-    multiplication2 = Number(multiplication2);
-    product = multiplication1 * multiplication2;
-    document.getElementById("multiplicationLabel").innerHTML = product;
-}
-
-let division1;
-let division2;
-let quotient;
-
-document.getElementById("divisionButton").onclick = function(){
-    division1 = document.getElementById("division1").value;
-    division2 = document.getElementById("division2").value;
-    division1 = Number(division1);
-    division2 = Number(division2);
-    quotient = division1 / division2;
-    document.getElementById("divisionLabel").innerHTML = quotient;
-}
-
-let rounding1;
-let roundingAnswer;
-
-document.getElementById("roundingButton").onclick = function(){
-    rounding1 = document.getElementById("rounding1").value;
-    rounding1 = Number(division1);
-    roundingAnswer = math.round(rounding1)
-    document.getElementById("roundingLabel").innerHTML = roundingAnswer;
+document.getElementById("button5").onclick = function(){
+    seven = document.getElementById("button5")
+    document.getElementById("calculatorLabel").innerHTML = seven;
 }
